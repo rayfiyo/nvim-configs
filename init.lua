6a4ab1597348c8f524d1bb2717696cb702858729
rayfiyo <108730891+rayfiyo@users.noreply.github.com>
2023-08-18 01:07:39 +0900

    initial commit🌱

---
+++ init.lua
@@ -0,0 +1,9 @@
+require "base"
+require "1packer_nvim"
+require "keymap"
+require "vim_opt"
+
+require "userPlugs/shortScript"
+require "userPlugs/nvim-treesitter"
+require "userPlugs/goLSP"
+require "impatient"
