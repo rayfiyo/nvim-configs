59ba31df9d97244ad327c28034d7d152db69dc2b
rayfiyo <rayfiyorex@gmail.com>
2025-01-03 16:26:30 +0900

    refactor: 💡 vim opt の各種説明や並び替えなど

--- lua/vim_opt.lua
+++ lua/vim_opt.lua
@@ -1,42 +1,38 @@
--- 2023-01-22
--- vim.opt.* の設定
 -- https://zenn.dev/hisasann/articles/neovim-settings-to-lua
 
 local opt = {
-	encoding = "utf-8",
-	fileencoding = "utf-8",
-	fileencodings = "utf-8,utf-16",
-	fileformats = "unix,dos,mac",
-
-	number = true,
-	shiftwidth = 4,
-	smartindent = true,
-	expandtab = true,
-	tabstop = 4,
-
-	cursorline = true,
-	ruler = true,
-
-	hlsearch = true,
-	incsearch = true,
-	showcmd = true,
-
-	splitbelow = true,
-	splitright = false,
-
-	clipboard = "unnamedplus",
-	mouse = "a",
-	shell = "fish",
-	swapfile = false,
-
+	-- UI
+	cursorline = true, -- カーソルが存在する行にハイライト
+	number = true, -- 行番号
+	ruler = true, -- カーソル位置
+	showcmd = true, -- 入力中のコマンド表示
 	termguicolors = false, -- 背景色をターミナルの背景色と同期
 
-	-- textwidth = 1,
+	-- エンコーディング，ファイル設定
+	encoding = "utf-8", -- neovim 内部の文字エンコーディング
+	fileencoding = "utf-8", -- 保存時のエンコーディング
+	fileencodings = "utf-8,utf-16,cp932", -- 読み込み時に試行するエンコーディング
+	fileformats = "unix,dos,mac", -- 試行する改行コード
+	swapfile = false, -- スワップファイル
+
+	-- インデント，タブ
+	expandtab = true, -- タブ文字を tabstop 個の半角スペースに置換
+	shiftwidth = 4, -- 挿入するインデントの幅
+	smartindent = true, -- 改行時にインデント
+	tabstop = 4, -- タブ文字の表示幅
+
+	-- 検索
+	hlsearch = true, -- 検索結果のハイライト
+	incsearch = true, -- 文字を入力する度に検索
+
+	-- その他
+	clipboard = "unnamedplus", -- "+ と "* レジスタをクリップボードと同期
+	mouse = "a", -- すべてのモードでマウス操作を有効化
+	shell = "fish", -- デフォルトシェル
+	splitbelow = true, -- 水平分割の新規ウィンドウを現在の下にする (true)
+	splitright = false, -- 垂直分割の新規ウィンドウを現在の左にする (false)
 }
 
 for i, v in pairs(opt) do
 	vim.opt[i] = v
 end
-
--- https://blog.atusy.net/2022/02/21/nvim-init-lua/
--- vim.opt.clipboard:append{'unnamedplus'}
