bbb453a7a597f0c1773f60f793acfb9f00c90417
rayfiyo <rayfiyorex@gmail.com>
2025-01-07 04:07:25 +0900

    feat: 🍻 やっぱり括弧のハイライトできた！！

--- lua/vim_g.lua
+++ lua/vim_g.lua
@@ -1,14 +1,14 @@
--- 0 で有効化
+-- 0 で有効化，紛らわしいので加えてコメントアウトも
 -- 1 で無効化
 
 local g = {
 	-- 高速化
 	-- https://zenn.dev/kawarimidoll/articles/8172a4c29a6653
 	did_install_default_menus = 1,
-	did_install_syntax_menu = 1,
+	-- did_install_syntax_menu = 0, -- matchparen 有効化のため
 	did_indent_on = 1,
 	did_load_ftplugin = 1,
-	-- did_load_filetypes = 1, -- lsp が動作しなくなる
+	-- did_load_filetypes = 0, -- lsp が動作しなくなる
 
 	-- https://coralpink.github.io/commentary/outro/lazy.html
 	-- disabled_plugins の設定 含む
@@ -16,7 +16,7 @@ local g = {
 	loaded_gzip = 1, -- Gzip圧縮ファイルの編集
 	loaded_man = 1, -- マニュアルページを表示する
 	loaded_matchit = 1, -- % キーの強化
-	loaded_matchparen = 1, -- 括弧系のハイライト
+	-- loaded_matchparen = 0, -- 括弧系のハイライト
 	loaded_netrwPlugin = 1, -- ファイルブラウザ機能
 	loaded_remote_plugins = 1, -- リモートプラグイン（PythonやRubyのインターフェース）
 	loaded_shada_plugin = 1, -- セッションデータ管理用のプラグイン
