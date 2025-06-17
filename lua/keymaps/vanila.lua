8fdbd95ec5f51268977680fc67868a578d6bc5eb
rayfiyo <rayfiyorex@gmail.com>
2025-06-17 12:00:18 +0900

    feat: 🍻 python の F5

--- lua/keymaps/vanila.lua
+++ lua/keymaps/vanila.lua
@@ -17,14 +17,14 @@ vim.g.maplocalleader = " "
 
 -- マッピング
 map("n", "<ESC><ESC>", "<cmd>noh<CR>") -- Escキー をタブルクリックで、ハイライト削除
-map("n", ":wq！", ":wq!")   -- <cmd>wq！用
-map("n", ":q！", ":q!")     -- <cmd>q！用
-map("n", "っd", "dd")       -- 全角・半角間違い用
-map("n", "っy", "yy")       -- 全角・半角間違い用
-map("n", "うう", "uu")      -- 全角・半角間違い用
-map("n", "い", "i")         -- 全角・半角間違い用
-map("n", "あ", "a")         -- 全角・半角間違い用
-map("n", "お", "o")         -- 全角・半角間違い用
+map("n", ":wq！", ":wq!") -- <cmd>wq！用
+map("n", ":q！", ":q!") -- <cmd>q！用
+map("n", "っd", "dd") -- 全角・半角間違い用
+map("n", "っy", "yy") -- 全角・半角間違い用
+map("n", "うう", "uu") -- 全角・半角間違い用
+map("n", "い", "i") -- 全角・半角間違い用
+map("n", "あ", "a") -- 全角・半角間違い用
+map("n", "お", "o") -- 全角・半角間違い用
 map({ "n", "i" }, "<F2>", "<Esc>:w<CR>:!xsel -bi < %<CR>")
 map({ "n", "i" }, "<F6>", "<Esc>:w<CR>:!go test %<CR>")
 map({ "n", "i" }, "<F5>", function()
@@ -35,6 +35,9 @@ map({ "n", "i" }, "<F5>", function()
 	elseif vim.bo.filetype == "go" then
 		vim.cmd("update")
 		vim.cmd("!echo : && go run %")
+	elseif vim.bo.filetype == "python" then
+		vim.cmd("update")
+		vim.cmd("!python %")
 	elseif vim.bo.filetype == "typst" then
 		vim.cmd("update")
 		vim.cmd("!TYPST_FONT_PATHS=/usr/share/fonts/ typst compile %")
